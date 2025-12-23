3f5c60d763377baaeae65f6d857b1e66ada6ac34
Akame08 <liam25@tutamail.com>
2025-12-23 05:52:19 +0100

    Fix: Eliminar carpeta 404 duplicada definitivamente

--- src/app/404/layout.js
+++
@@ -1,8 +0,0 @@
-export default function Layout404({ children }) {
-  return (
-    <div className="antidote-layout">
-      {children}
-    </div>
-  );
-}
-
--- src/app/404/page.js
+++
@@ -1,9 +0,0 @@
-export default function Page404() {
-  return (
-    <div className="p-20 text-center text-white">
-      <h1 className="text-4xl font-bold">404</h1>
-      <p>Página no encontrada (Ruta manual)</p>
-    </div>
-  );
-}
-
